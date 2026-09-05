a25f350dc152ccc3bd4fc431d4e76055a150aeae
Maysha082 <251-35-082@diu.edu.bd>
2026-09-05 21:07:47 +0600

    Initial Java project

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[]args){
+
+      int a = 3;
+      int b = 4;
+      int c = 5;
+      float avg = (a+b+c)/3;
+
+        System.out.println("Avarage: " + avg );
+
+    }
+
+}
+
---
+++ src/info.java
@@ -0,0 +1,17 @@
+public class info {
+    public static void main(String[]args){
+
+
+        String name = "Ant";
+        int age = 23;
+        double cgpa = 3.5;
+        char grade = 'B';
+
+
+        System.out.println("Name: " + name );
+        System.out.println("Age: " + age );
+        System.out.println("CGPA: " + cgpa );
+        System.out.println("Grade: " + grade);
+    }
+
+}
